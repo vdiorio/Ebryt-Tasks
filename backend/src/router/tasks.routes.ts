6438e4fc44cec63bbc8185b0b102fd4e04e8fd74
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-19 19:05:18 -0300

    :sparkles: ROUTER CRUD - Update
    Criação da rota de update de tasks

--- backend/src/router/tasks.routes.ts
+++ backend/src/router/tasks.routes.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/', controller.task.createTask);
 router.get('/', controller.task.getAll);
+router.put('/:id', controller.task.updateTask);
 
 export default router;
