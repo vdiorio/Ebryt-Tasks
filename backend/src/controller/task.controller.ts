a1219b3c19390dfecc8b833d8c8146aa7928d572
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-19 18:48:57 -0300

    :sparkles: CONTROLLER CRUD - Update Task
    Criação da função update task para o controller de task

--- backend/src/controller/task.controller.ts
+++ backend/src/controller/task.controller.ts
@@ -8,4 +8,11 @@ export default class TaskController {
     const newTask = await services.task.createTask(task);
     return res.status(StatusCodes.OK).json(newTask);
   };
+
+  static updateTask = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const fieldsToUpdate = req.body;
+    const task = await services.task.updateTask(id, fieldsToUpdate);
+    return res.status(StatusCodes.OK).json(task);
+  };
 }
