3aa63f8709271aad7de2f8d417d78e758339a9d8
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-19 19:06:35 -0300

    :sparkles: Implementação da rota /tasks na aplicação

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import express, { Response, Request } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import router from './router';
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(bodyParser.json());
 app.get('/', (_req: Request, res: Response) => (
   res.status(StatusCodes.OK).json('Bem vindo ao backend de Ebyrt Tasks!')
 ));
+
+app.use('/tasks', router.tasks);
 
 export default app;
