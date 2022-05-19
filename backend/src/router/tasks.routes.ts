d688fdfcdbcfe963bd7b75396d2a80920fc428d3
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-19 19:04:38 -0300

    :sparkles: ROUTER CRUD - Read
    Criação da rota de leitura de tasks

--- backend/src/router/tasks.routes.ts
+++ backend/src/router/tasks.routes.ts
@@ -4,5 +4,6 @@ import controller from '../controller';
 const router = express.Router();
 
 router.post('/', controller.task.createTask);
+router.get('/', controller.task.getAll);
 
 export default router;
