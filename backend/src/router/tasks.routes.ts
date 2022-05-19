46e42a33d16376d169fafb9a35328c4157930380
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-19 20:07:31 -0300

    :sparkles: Implementação de middlewares
    Implementação de middlewares de validação a aplicação principal

--- backend/src/router/tasks.routes.ts
+++ backend/src/router/tasks.routes.ts
@@ -1,11 +1,23 @@
 import express from 'express';
 import controller from '../controller';
+import middlewares from '../middlewares';
 
 const router = express.Router();
 
-router.post('/', controller.task.createTask);
+router.post(
+  '/',
+  middlewares.task.validateNewTask,
+  controller.task.createTask,
+);
+
 router.get('/', controller.task.getAll);
-router.put('/:id', controller.task.updateTask);
+
+router.put(
+  '/:id',
+  middlewares.task.validateUpdateTask,
+  controller.task.updateTask,
+);
+
 router.delete('/:id', controller.task.deleteTask);
 
 export default router;
