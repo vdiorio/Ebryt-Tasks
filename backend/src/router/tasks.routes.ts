88ad06a437f506e2bdf35665f37e1d6a46cb4403
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-19 19:05:52 -0300

    :sparkles: ROUTER CRUD - Delete
    Criação da rota de exclusão de tasks

--- backend/src/router/tasks.routes.ts
+++ backend/src/router/tasks.routes.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/', controller.task.createTask);
 router.get('/', controller.task.getAll);
 router.put('/:id', controller.task.updateTask);
+router.put('/:id', controller.task.deleteTask);
 
 export default router;
