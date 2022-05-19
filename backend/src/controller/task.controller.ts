b7d2f288bd80bc47ad4556fe3d944704bb527703
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-19 18:51:04 -0300

    :sparkles: CONTROLLER CRUD - READ Tasks
    Criação da função getAll para o controller de tasks.

--- backend/src/controller/task.controller.ts
+++ backend/src/controller/task.controller.ts
@@ -9,6 +9,11 @@ export default class TaskController {
     return res.status(StatusCodes.OK).json(newTask);
   };
 
+  static getAll = async (_req: Request, res: Response) => {
+    const tasks = await services.task.getAllTasks();
+    return res.status(StatusCodes.OK).json(tasks);
+  };
+
   static updateTask = async (req: Request, res: Response) => {
     const { id } = req.params;
     const fieldsToUpdate = req.body;
