87861bcde236f4ae46a203debc5029fdf9d41f93
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-19 20:05:48 -0300

    :sparkles: Validação de nova task
    Criação de middleware de validação para novas tasks

--- backend/src/middlewares/task.middlewares.ts
+++ backend/src/middlewares/task.middlewares.ts
@@ -12,4 +12,13 @@ export default class TaskMiddlewares {
     title: Joi.string().min(1),
     status: Joi.string().valid('Pronto', 'Em andamento', 'Pendente'),
   }).or('title', 'status').required();
+
+  static validateNewTask = async (req: Request, res: Response, next: NextFunction) => {
+    const { error } = this.allRequired.validate(req.body);
+    if (error) {
+      const { message } = error.details[0];
+      return res.status(StatusCodes.BAD_REQUEST).json({ message });
+    }
+    return next();
+  };
 }
