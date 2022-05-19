5b48acfe91945dfc3711809b66edfc4b9d351f30
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-19 18:06:17 -0300

    :sparkles: BodyParser
    Adiciona body-parser a aplicação

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
+import bodyParser from 'body-parser';
 import express, { Response, Request } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (_req: Request, res: Response) => (
   res.status(StatusCodes.OK).json('Bem vindo ao backend de Ebyrt Tasks!')
