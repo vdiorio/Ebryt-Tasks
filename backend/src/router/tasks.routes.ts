6c477d3b826a2966fd94016484f5e1230e0d65e8
Vitor Diorio <vitor.diorio@yahoo.com.br>
2022-05-19 19:03:49 -0300

    :sparkles: ROUTER CRUD - Create
    Criação da rota de criação de tasks

--- backend/src/router/tasks.routes.ts
+++ backend/src/router/tasks.routes.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import controller from '../controller';
 
 const router = express.Router();
+
+router.post('/', controller.task.createTask);
 
 export default router;
